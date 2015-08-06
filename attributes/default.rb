d7ef7d98bdc333680bc38ea961c27ecf023d67c1
Steven Haddox <steven@haddox.us>
2015-08-06 01:11:58 -0400

    Fix attribute defaults

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+default['rvm_fw']['path'] = '/usr/local/rvm'
+default['rvm_fw']['sudo'] = true
+default['rvm_fw']['url'] = nil
+default['rvm_fw']['version'] = '1.18.14'
+default['rvm_fw']['global_ruby'] = 'ruby-2.2.2'
