75a5acd387e654303a88cf254a4d5569152aadbf
Steven Haddox <steven@haddox.us>
2015-08-14 17:25:53 -0400

    version bump

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs common ruby packages and RVM via RVM::FW'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/stevenhaddox/cookbook-rvm_fw'
 issues_url 'https://github.com/stevenhaddox/cookbook-rvm_fw/issues'
-version '0.2.0'
+version '0.2.1'
 
 recipe 'rvm_fw::default', 'Install RVM via RVM::FW server'
 
