4305e6367214798203c467f339c45e93768c24bd
Steven Haddox <steven@haddox.us>
2015-11-04 00:34:39 -0500

    kitchen verify working with centos

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,16 +1,17 @@
 require 'spec_helper'
 
+# User install
 describe 'rvm_fw::default' do
-  describe command('bash -c "source /usr/local/rvm/scripts/rvm && which ruby"') do
+  describe command('bash -c "source /home/vagrant/.rvm/scripts/rvm && which ruby"') do
     it "executes without error" do
       expect(subject.exit_status).to eq 0
     end
     it "is the default RVM ruby" do
-      expect(subject.stdout).to match(/\/usr\/local\/rvm\/rubies\/ruby-2.2.2\/bin\/ruby\n/)
+      expect(subject.stdout).to match(/\/home\/vagrant\/.rvm\/rubies\/ruby-2.2.2\/bin\/ruby\n/)
     end
   end
 
-  describe command('bash -c "source /usr/local/rvm/scripts/rvm && rvm --version"') do
+  describe command('bash -c "source /home/vagrant/.rvm/scripts/rvm && rvm --version"') do
     it "executes without error" do
       expect(subject.exit_status).to eq 0
     end
@@ -19,7 +20,7 @@ describe 'rvm_fw::default' do
     end
   end
 
-  describe command('/usr/local/rvm/wrappers/default/ruby --version') do
+  describe command('/home/vagrant/.rvm/wrappers/default/ruby --version') do
     it "executes without error" do
       expect(subject.exit_status).to eq 0
     end
@@ -28,9 +29,46 @@ describe 'rvm_fw::default' do
     end
   end
 
-  describe command('/usr/local/rvm/wrappers/default/gem list bundler') do
+  describe command('/home/vagrant/.rvm/wrappers/default/gem list bundler') do
     it "executes without error" do
       expect(subject.exit_status).to eq 0
     end
   end
 end
+
+#TODO: Verify system-wide install works someday
+#describe 'rvm_fw::default' do
+#  describe command('bash -c "source /usr/local/rvm/scripts/rvm && which ruby"') do
+#    it "executes without error" do
+#      expect(subject.exit_status).to eq 0
+#    end
+#    it "is the default RVM ruby" do
+#      expect(subject.stdout).to match(/\/usr\/local\/rvm\/rubies\/ruby-2.2.2\/bin\/ruby\n/)
+#    end
+#  end
+#
+#  describe command('bash -c "source /usr/local/rvm/scripts/rvm && rvm --version"') do
+#    it "executes without error" do
+#      expect(subject.exit_status).to eq 0
+#    end
+#    it "is the right version" do
+#      expect(subject.stdout).to match(/rvm 1.18.14/)
+#    end
+#  end
+#
+#  describe command('/usr/local/rvm/wrappers/default/ruby --version') do
+#    it "executes without error" do
+#      expect(subject.exit_status).to eq 0
+#    end
+#    it "is the right version" do
+#      expect(subject.stdout).to match(/ruby 2.2.2/)
+#    end
+#  end
+#
+#  describe command('/usr/local/rvm/wrappers/default/gem list bundler') do
+#    it "executes without error" do
+#      expect(subject.exit_status).to eq 0
+#    end
+#  end
+#end
+#
