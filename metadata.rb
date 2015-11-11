43e5f808915c86641e2de0d1c3e5483a59fd20a0
Steven Haddox <steven@haddox.us>
2015-11-11 12:03:42 -0500

    metadata changes

--- metadata.rb
+++ metadata.rb
@@ -6,16 +6,16 @@ description 'Installs common ruby packages and RVM via RVM::FW'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/stevenhaddox/cookbook-rvm_fw'
 issues_url 'https://github.com/stevenhaddox/cookbook-rvm_fw/issues'
-version '0.2.4'
+version '0.3.0'
 
 recipe 'rvm_fw::default', 'Install RVM via RVM::FW server'
 
 depends 'build-essential', '~> 2'
 depends 'apt', '~> 2.7'
 
-supports 'centos'
-supports 'fedora'
-supports 'redhat'
-supports 'debian'
-supports 'ubuntu'
-supports 'scientific'
+supports 'centos', '>=6.0'
+#supports 'fedora'
+#supports 'redhat'
+#supports 'debian'
+#supports 'ubuntu'
+#supports 'scientific'
