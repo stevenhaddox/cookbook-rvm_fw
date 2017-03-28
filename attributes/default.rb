909b4c0571bf366ba477602dfc86449e68cb3302
Steven Haddox <steven@haddox.us>
2017-03-28 01:33:59 -0400

    Update default ruby

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['rvm_fw']['url'] = nil
 default['rvm_fw']['version'] = '1.18.14'
-default['rvm_fw']['global_ruby'] = 'ruby-2.2.3'
+default['rvm_fw']['global_ruby'] = 'ruby-2.3.1'
 default['rvm_fw']['extra_rubies'] = []
 default['rvm_fw']['disable_requiretty'] = false
 default['rvm_fw']['user'] = 'root'
