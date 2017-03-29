606d5f438b08dd57fbf22cf6c7a8bccda521073d
Steven Haddox <steven@haddox.us>
2017-03-29 01:50:21 -0400

    Remove dependency version locking for flexibility

--- metadata.rb
+++ metadata.rb
@@ -6,12 +6,12 @@ description 'Installs common ruby packages and RVM via RVM::FW'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/stevenhaddox/cookbook-rvm_fw'
 issues_url 'https://github.com/stevenhaddox/cookbook-rvm_fw/issues'
-version '0.5.0'
+version '1.0.0'
 
 recipe 'rvm_fw::default', 'Install RVM via RVM::FW server'
 
-depends 'build-essential', '~> 7.0'
-depends 'apt', '~> 6.0.1'
+depends 'build-essential'
+depends 'apt'
 
 supports 'centos'
 supports 'fedora'
